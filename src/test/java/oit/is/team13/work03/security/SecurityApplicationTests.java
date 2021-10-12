ab251c71b399cfe2998c5ac66eb609f59c2c3927
e1b19058 <e1b19058@oit.ac.jp>
2021-10-12 14:09:36 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/team13/work03/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team13.work03.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
